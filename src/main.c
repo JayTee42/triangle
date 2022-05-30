75fe6cb6b05150ad7f25e329935ca5ff11020ef2
Jonas Treumer <endoplasmaticreticulum@yahoo.de>
2022-05-30 11:37:53 +0200

    Creating the window

--- src/main.c
+++ src/main.c
@@ -5,7 +5,8 @@
 
 void error_callback(int error, const char* description)
 {
-    // TODO
+    fprintf(stderr, "Error: %s (%d)\n", description, error);
+    exit(EXIT_FAILURE);
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int fb_width, int fb_height)
@@ -21,5 +22,32 @@ void window_size_callback(GLFWwindow* window, int width, int height)
 int main(void)
 {
     printf("Hello triangle!\n");
+
+    // Init GLFW
+    printf("Initializing GLFW ...\n");
+
+    glfwSetErrorCallback(error_callback);
+    check_error(glfwInit(), "Failed to initialize GLFW :(\n");
+
+    // We want at least OpenGL 4.1.
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+    // Create the window itself.
+    printf("Creating window ...\n");
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello triangle", NULL, NULL);
+    check_error(window != NULL, "Failed to create window");
+
+    // TODO
+
+    // Destroy the window.
+    glfwDestroyWindow(window);
+
+    // Terminate GLFW in the end.
+    glfwTerminate();
+
     return 0;
 }
